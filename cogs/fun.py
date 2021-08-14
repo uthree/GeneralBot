2f1af18e887c28415d7ca68745ef820641825d02
Assault-8448 <vvvvvgaga12356@yahoo.co.jp>
2021-08-14 10:43:19 +0900

    fix: fun.py

--- cogs/fun.py
+++ cogs/fun.py
@@ -1,5 +1,5 @@
-import discord.ext.commands as commands
-import discord
+from discord.ext import commands
+import discord as discord
 import random
 
 # Fun(ネタコマンド系) cog
@@ -8,8 +8,8 @@ class Fun(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.commands(aliases=["sintyoku"]
-            async def sinchoku(self,ctx):
+    @commands.command(aliases=["sintyoku"])
+    async def sinchoku(self,ctx):
             links = [
                 "https://cdn.discordapp.com/attachments/708828790796845189/787409537883439134/alicedoudesuka-300x168.jpg",
                 "https://cdn.discordapp.com/attachments/787413181277798400/787413202958417920/images-6.jpeg",
@@ -17,7 +17,7 @@ class Fun(commands.Cog):
                 "https://cdn.discordapp.com/attachments/787413181277798400/787413268872429568/images.jpeg",
                 "https://cdn.discordapp.com/attachments/787413181277798400/787413301776089118/main.pngcompresstrue.png",
                 "https://cdn.discordapp.com/attachments/787413181277798400/787413247686737930/images-3.jpeg"
-            ]
+                ]
             await ctx.send(random.choice(links))
 
 def setup(bot):
